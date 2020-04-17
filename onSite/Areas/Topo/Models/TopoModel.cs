d7a828c1d6ff07a5257ec2f5f71e5d0c27736f7b
Clytra <48762967+Clytra@users.noreply.github.com>
2020-04-17 22:15:47 +0200

    dodanie do modelu metadanych

--- onSite/Areas/Topo/Models/TopoModel.cs
+++ onSite/Areas/Topo/Models/TopoModel.cs
@@ -1,12 +1,24 @@
-﻿namespace onSite.Areas.Topo.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace onSite.Areas.Topo.Models
 {
     public class TopoModel
     {
         public int TopoID { get; set; }
+
+        [Required(ErrorMessage = "To pole nie może zostać puste.")]
         public string Voivodeship { get; set; }
+
+        [Required(ErrorMessage = "To pole nie może zostać puste.")]
         public string Area { get; set; }
+
+        [Required(ErrorMessage = "To pole nie może zostać puste.")]
         public string Rock { get; set; }
+
+        [Required(ErrorMessage = "To pole nie może zostać puste.")]
         public string ClimbingRoute { get; set; }
+
+        [Required(ErrorMessage = "To pole nie może zostać puste.")]
         public string Difficulty { get; set; }
 
         public string Description { get; set; }
