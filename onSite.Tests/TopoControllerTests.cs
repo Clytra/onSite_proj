253d6331c1f27bfc559b95a6ca6e2bef551cc4bd
Clytra <48762967+Clytra@users.noreply.github.com>
2020-04-18 21:07:25 +0200

    test jednostkowy - dane stronicowania w widoku modelu

--- onSite.Tests/TopoControllerTests.cs
+++ onSite.Tests/TopoControllerTests.cs
@@ -1,6 +1,7 @@
 using Moq;
 using onSite.Areas.Topo.Controllers;
 using onSite.Areas.Topo.Models;
+using onSite.Areas.Topo.Models.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -9,6 +10,39 @@ namespace onSite.Tests
 {
     public class TopoControllerTests
     {
+        [Fact]
+        public void Can_Send_Pagination_View_Model()
+        {
+            //Przygotowanie
+            Mock<ITopoRepository> mock = new Mock<ITopoRepository>();
+            mock.Setup(m => m.Topos).Returns((new TopoModel[]
+            {
+                new TopoModel {TopoID = 1, Voivodeship = "Województwo1", Area = "Obszar1", Region = "Region1", Sector = "Sektor1", Rock = "Ska³a1", Wall = "Œciana1"},
+                new TopoModel {TopoID = 2, Voivodeship = "Województwo2", Area = "Obszar2", Region = "Region2", Sector = "Sektor2", Rock = "Ska³a2", Wall = "Œciana2"},
+                new TopoModel {TopoID = 3, Voivodeship = "Województwo3", Area = "Obszar3", Region = "Region3", Sector = "Sektor3", Rock = "Ska³a3", Wall = "Œciana3"},
+                new TopoModel {TopoID = 4, Voivodeship = "Województwo4", Area = "Obszar4", Region = "Region4", Sector = "Sektor4", Rock = "Ska³a4", Wall = "Œciana4"},
+                new TopoModel {TopoID = 5, Voivodeship = "Województwo5", Area = "Obszar5", Region = "Region5", Sector = "Sektor5", Rock = "Ska³a5", Wall = "Œciana5"},
+                new TopoModel {TopoID = 6, Voivodeship = "Województwo6", Area = "Obszar6", Region = "Region6", Sector = "Sektor6", Rock = "Ska³a6", Wall = "Œciana6"},
+                new TopoModel {TopoID = 7, Voivodeship = "Województwo7", Area = "Obszar7", Region = "Region7", Sector = "Sektor7", Rock = "Ska³a7", Wall = "Œciana7"},
+            }).AsQueryable<TopoModel>());
+
+            //Przygotowanie
+            TopoController controller =
+                new TopoController(mock.Object) { PageSize = 3 };
+
+            //Dzia³anie
+            TopoListViewModel result =
+                controller.List(2).ViewData.Model as TopoListViewModel;
+
+            //Asercje
+            PagingInfo pageInfo = result.PagingInfo;
+            Assert.Equal(2, pageInfo.CurrentPage);
+            Assert.Equal(3, pageInfo.RecordsPerPage);
+            Assert.Equal(5, pageInfo.TotalRecords);
+            Assert.Equal(2, pageInfo.TotalPages);
+        }
+
+
         [Fact]
         public void Can_Paginate()
         {
@@ -29,10 +63,10 @@ namespace onSite.Tests
             controller.PageSize = 3;
 
             //Dzia³anie
-            IEnumerable<TopoModel> result = controller.List(2).ViewData.Model as IEnumerable<TopoModel>;
+            TopoListViewModel result = controller.List(2).ViewData.Model as TopoListViewModel;
 
             //Asercje
-            TopoModel[] topoArray = result.ToArray();
+            TopoModel[] topoArray = result.Topos.ToArray();
             Assert.True(topoArray.Length == 2);
             Assert.Equal("Województwo4", topoArray[0].Voivodeship);
             Assert.Equal("Województwo5", topoArray[1].Voivodeship);
