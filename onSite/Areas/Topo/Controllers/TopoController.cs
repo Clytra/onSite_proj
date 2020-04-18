42627cee1df9d4337aef556567f0f045a5b1f8a4
Clytra <48762967+Clytra@users.noreply.github.com>
2020-04-18 10:59:05 +0200

    dodanie kontrolera do modelu TOPO

---
+++ onSite/Areas/Topo/Controllers/TopoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using onSite.Areas.Topo.Models;
+
+namespace onSite.Areas.Topo.Controllers
+{
+    public class TopoController : Controller
+    {
+        private ITopoRepository repository;
+
+        public TopoController(ITopoRepository repo)
+        {
+            repository = repo;
+        }
+        public ViewResult List() => View(repository.Topos);
+    }
+}
