901ec9dbbd162673ecca6bd083812459e3089158
Clytra <48762967+Clytra@users.noreply.github.com>
2020-04-18 19:26:57 +0200

    dodanie klasy atrybutu pomocniczego znacznika

---
+++ onSite/Areas/Topo/Models/ViewModels/PagingInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onSite.Areas.Topo.Models.ViewModels
+{
+    public class PagingInfo
+    {
+        public int TotalRecords { get; set; }
+        public int RecordsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+
+        public int TotalPages =>
+            (int)Math.Ceiling((decimal)TotalRecords / RecordsPerPage);
+    }
+}
