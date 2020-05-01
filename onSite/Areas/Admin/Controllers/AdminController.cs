29b841812189913f107d8ca2f168179747fc93cf
Clytra <48762967+Clytra@users.noreply.github.com>
2020-05-01 14:21:29 +0200

    Stworzenie kontrolera do Admina

---
+++ onSite/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using onSite.Areas.Topo.Models;
+
+namespace onSite.Areas.Admin.Controllers
+{
+    public class AdminController : Controller
+    {
+        private ITopoRepository _repository;
+
+        public AdminController(ITopoRepository repo)
+        {
+            _repository = repo;
+        }
+
+        public ViewResult TopoList()
+            => View(_repository.Topos);
+
+        public ViewResult RouteList()
+            => View(_repository.Topos);
+    }
+}
