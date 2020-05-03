5c3a53d205de372dece5a53474dba22faec324a5
Clytra <48762967+Clytra@users.noreply.github.com>
2020-05-03 17:32:17 +0200

    Dodanie do AdminController repozytorium routes

--- onSite/Areas/Admin/Controllers/AdminController.cs
+++ onSite/Areas/Admin/Controllers/AdminController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using onSite.Areas.Topo.Models;
 using onSite.Areas.Topo.Models.ViewModels;
+using onSite.Repository.Topo;
 using System.Linq;
 
 namespace onSite.Areas.Admin.Controllers
@@ -8,21 +9,23 @@ namespace onSite.Areas.Admin.Controllers
     [Area("Admin")]
     public class AdminController : Controller
     {
-        private ITopoRepository _repository;
+        private ITopoRepository _topoRepository;
+        private IRouteRepository _routeRepository;
 
-        public AdminController(ITopoRepository repo)
+        public AdminController(ITopoRepository tRepo, IRouteRepository rRepo)
         {
-            _repository = repo;
+            _topoRepository = tRepo;
+            _routeRepository = rRepo;
         }
 
         public ViewResult TopoList()
             => View(new TopoListViewModel
             {
-                Topos = _repository.Topos
+                Topos = _topoRepository.Topos
             });
 
-        public ViewResult Edit(int topoId)
-            => View(_repository.Topos
-                .FirstOrDefault(t => t.TopoID == topoId));
+        public ViewResult Edit(int routeId)
+            => View(_routeRepository.Routes
+                .FirstOrDefault(t => t.RouteID == routeId));
     }
 }
