1872cd8cda42c4e885760bad774d0c09121fbf4c
Clytra <48762967+Clytra@users.noreply.github.com>
2020-05-03 17:18:14 +0200

    Dodanie repozytorium do modelu ClimbingRouteModel

---
+++ onSite/Repository/Topo/EFRouteRepository.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using onSite.Areas.Topo.Models;
+using onSite.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace onSite.Repository.Topo
+{
+    public class EFRouteRepository : IRouteRepository
+    {
+        private ApplicationDbContext context;
+
+        public EFRouteRepository(ApplicationDbContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IEnumerable<ClimbingRouteModel> Routes => context.Routes
+            .Include(r => r.TopoModel);
+
+        public ClimbingRouteModel DeleteRoute(int routeID)
+        {
+            ClimbingRouteModel dbEntry = context.Routes
+                .FirstOrDefault(p => p.RouteID == routeID);
+            if (dbEntry != null)
+            {
+                context.Routes.Remove(dbEntry);
+                context.SaveChanges();
+            }
+            return dbEntry;
+        }
+    }
+}
+
---
+++ onSite/Repository/Topo/IRouteRepository.cs
@@ -0,0 +1,12 @@
+﻿using onSite.Areas.Topo.Models;
+using System.Collections.Generic;
+
+namespace onSite.Repository.Topo
+{
+    public interface IRouteRepository
+    {
+        IEnumerable<ClimbingRouteModel> Routes { get; }
+
+        ClimbingRouteModel DeleteRoute(int RouteId);
+    }
+}
