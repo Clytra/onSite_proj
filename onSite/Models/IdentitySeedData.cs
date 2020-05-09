4879a50ac9d9da7c39846286b5fb3aa451fa50d8
Clytra <48762967+Clytra@users.noreply.github.com>
2020-05-09 13:23:25 +0200

    Zdefiniowanie danych początkowych systemu Identity

---
+++ onSite/Models/IdentitySeedData.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace onSite.Models
+{
+    public class IdentitySeedData
+    {
+        private const string Admin = "Admin";
+        private const string AdminPassword = "sfgd86svs1";
+
+        public static async void EnsurePopulated(IApplicationBuilder app)
+        {
+            UserManager<IdentityUser> userManager = app.ApplicationServices
+                .GetRequiredService<UserManager<IdentityUser>>();
+
+            IdentityUser user = await userManager.FindByIdAsync(Admin);
+            if (user == null)
+            {
+                user = new IdentityUser("Admin");
+                await userManager.CreateAsync(user, AdminPassword);
+            }
+        }
+    }
+}
