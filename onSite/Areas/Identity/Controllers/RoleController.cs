87d85fea1e9900549af3c28029a5be0f64960784
Clytra <48762967+Clytra@users.noreply.github.com>
2020-05-11 19:39:04 +0200

    Tworzenie i usuwanie ról

--- onSite/Areas/Identity/Controllers/RoleController.cs
+++ onSite/Areas/Identity/Controllers/RoleController.cs
@@ -10,23 +10,20 @@ namespace onSite.Areas.Admin.Controllers
     [Area("Identity")]
     public class RoleController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        protected RoleManager<IdentityRole> roleManager { get; }
+        protected RoleManager<IdentityRole> _roleManager { get; }
         public RoleController(RoleManager<IdentityRole> _roleManager)
         {
-            roleManager = _roleManager;
+            _roleManager = _roleManager;
         }
+        public ViewResult Index() => View(_roleManager.Roles);
 
+        public IActionResult Add() => View();
         public async Task<IActionResult> Add(RoleViewModel viewModel)
         {
             if (ModelState.IsValid)
             {
                 var ir = new IdentityRole(viewModel.Name);
-                var result = await roleManager.CreateAsync(ir);
+                var result = await _roleManager.CreateAsync(ir);
                 if (result.Succeeded)
                 {
 
@@ -37,6 +34,37 @@ namespace onSite.Areas.Admin.Controllers
                 }
             }
             return View(viewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await _roleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "Nie znaleziono roli.");
+            }
+            return View("Index", _roleManager.Roles);
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
         }
     }
 }
